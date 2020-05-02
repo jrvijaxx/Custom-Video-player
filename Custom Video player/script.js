6de6b34401c4745ab385489a8a7dd8b82c976451
jrvijaxx <59405566+jrvijaxx@users.noreply.github.com>
2020-05-02 06:30:34 +0530

    Add files via upload

---
+++ Custom Video player/script.js
@@ -0,0 +1,66 @@
+const video = document.getElementById("video");
+const play = document.getElementById("play");
+const stop = document.getElementById("stop");
+const progress = document.getElementById("progress");
+const timestamp = document.getElementById("timpstamp");
+
+// Play & Pause Video
+function toggleVideoStatus() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+// update play/pause  icon
+function updatePlayIcon() {
+  if (video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa"></i>';
+  } else {
+    play.innerHTML = '<i class="fa fa-pause fa"></i>';
+  }
+}
+
+// update progress & timestamp
+function updateProgress() {
+  progress.value = (video.currentTime / video.duration) * 100;
+
+  // get the minutes
+  let mins = Math.floor(video.currentTime / 60);
+  if (mins < 10) {
+    mins = "0" + String(mins);
+  }
+
+  // get the seconds
+  let secs = Math.floor(video.currentTime % 60);
+  if (secs < 10) {
+    secs = "0" + String(secs);
+  }
+  console.log(secs);
+  timestamp.innerHTML = `${mins} : ${secs}`;
+}
+
+// set video time for progress
+function setVideoProgress() {
+  video.currentTime = (+progress.value * video.duration) / 100;
+}
+
+// stop video
+function stopVideo() {
+  video.currentTime = 0;
+  video.pause();
+}
+
+// Event Listeners
+
+video.addEventListener("click", toggleVideoStatus);
+video.addEventListener("pause", updatePlayIcon);
+video.addEventListener("play", updatePlayIcon);
+video.addEventListener("timeupdate", updateProgress);
+
+play.addEventListener("click", toggleVideoStatus);
+
+stop.addEventListener("click", stopVideo);
+
+progress.addEventListener("change", setVideoProgress);
